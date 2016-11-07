80067c864d282244ef78a6c7bd1943c9f3acfbf6
daniel-lozano <d.lozano343@uniandes.edu.co>
2016-11-07 16:29:06 -0500

    E>Vo

--- WKB_bellow.py
+++ WKB_bellow.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy import integrate
+import sys
 
 
 
@@ -20,6 +21,23 @@ def integrand(x,k,eta,hbar,m,a):
 
     return a*m*exp/div
 
+
+def integrand1(xt,x,k,eta,hbar,m,a):
+    
+    factor1= - 2*k*x
+    
+    factor2= - (eta/hbar)*pow(x,2)
+    
+    cte=np.exp(+ 2*k*xt+ (eta/hbar)*pow(xt,2))
+    
+    exp= np.exp(factor1+factor2)
+    
+    div= hbar*k + eta*x
+    
+    return cte*m*exp/div
+
+
+
 '''
 %%%%%%%%%%%%%%%%%%%% Variables usadas en el calculo %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 '''
@@ -28,9 +46,12 @@ def integrand(x,k,eta,hbar,m,a):
 #definiendo el espacio
 
 
+print("el argumneto es",sys.argv[0])
+
 #definiendo energias adimensionales
 Ne=100
-E=np.linspace(0.01,0.99,Ne)
+Eb=np.linspace(0.01,0.99,Ne)
+Ea=np.linspace(1.01,2.0,Ne)
 
 #Constantes usadas
 
@@ -50,27 +71,28 @@ Tenemos que tener que eta*l << hbar*k, para satisfacerlo tomaremos el k mas pequ
 '''
 
 
-eta=np.linspace(0,5E-11,2) # eV/Fermi
-
+#eta=np.linspace(0,5E-11,2) # eV/Fermi
+eta=[0,float(sys.argv[1])]
 print("eta=",eta)
 
 
-T=np.zeros(len(E))
+Tb=np.zeros(len(Eb))
+Ta=np.zeros(len(Eb))
 
 '''
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Comenzando el calculo %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 '''
 
-#defining the array for the intergration
 
+##solving for E bellow
 
 for j in range(len(eta)):
 
-    for i in range(len(E)):
+    for i in range(len(Eb)):
     
-        k=np.sqrt(2*m*Vo*(1-E[i]))/hbar
+        k=np.sqrt(2*m*Vo*(1-Eb[i]))/hbar
         
-        kp=np.sqrt(2*m*Vo*(E[i]))/hbar
+        kp=np.sqrt(2*m*Vo*(Eb[i]))/hbar
         
         N_factor= 2*np.sqrt(k*kp/(k**2+kp**2))
     
@@ -78,16 +100,69 @@ for j in range(len(eta)):
     
         resultados=integrate.quad(funcion,0,1,limit=100,limlst=96)
 
-        T[i]=(N_factor**2)*(resultados[0])* (6.58*10**(-22)/hbar)
+        Tb[i]=(N_factor**2)*(resultados[0])* (6.58*10**(-22)/hbar)
         
         nombre="$ \eta= $"+str(eta[j]) +"$  MeV $"
 
         
-    plt.plot(E,T,label=nombre)
+    plt.plot(Eb,Tb,label=nombre)
 
-for i in range(len(T)):
+##imrpimiento archivo
+
+f=open("bellow_wkb.dat","w")
+
+for i in range(len(Tb)):
     
-    print(E[i],T[i])
+    f.write(str(Eb[i])+str( )+str(Tb[i]))
+
+f.close()
+
+
+dE=np.zeros(len(Ea))
+
+
+for i in range(len(dE)):
+    dE[i]=eta[1]*np.sqrt(2*(Ea[i]-1)/(m*Vo))*a
+
+
+print("minimun dissipation ",min(dE))
+
+print("maximun dissipation ",max(dE))
+
+
+
+##solving for E above
+
+
+for j in range(len(eta)):
+    
+    for i in range(len(Ea)):
+        
+        k=np.sqrt(2*m*Vo*(Ea[i]-1))/hbar
+        
+        kp=np.sqrt(2*m*Vo*(Ea[i]))/hbar
+        
+        N_factor= 2*np.sqrt(k*kp/(k**2+kp**2))
+        
+        xturning=0#np.sqrt((Ea[i]-Vo)*m/eta[j])
+        
+        if(Ea[i]<1+max(dE) and eta[j]!=0):
+            xturning=np.sqrt((Ea[i]-Vo)*m/eta[j])
+        
+        
+        funcion1= lambda x: integrand1(xturning,x,k,eta[j],hbar,m,a)
+        
+        resultados=integrate.quad(funcion1,xturning,a,limit=100,limlst=96)
+        
+        Ta[i]=(N_factor**2)*(resultados[0])* (6.58*10**(-22)/hbar)
+    
+        nombre="$ \eta= $"+str(eta[j]) +"$  MeV $"
+        
+    
+    plt.plot(Ea,Ta,label=nombre)
+
+
+
 
 
 
